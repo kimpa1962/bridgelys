c2586169875e016102385044894f3e33a23e818f
Kim <hello@kimpa.se>
2026-03-19 21:26:36 +0100

    Sitemap all pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,22 +1,33 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://bridgelys.se',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://bridgelys.se/services/web-development',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://bridgelys.se/services/seo',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://bridgelys.se/services',
-      lastModified: new Date(),
-    },
+  const baseUrl = 'https://bridgelys.se'
+
+  const routes = [
+    '',
+    '/services/procurement',
+    '/services/project-management',
+    '/services/accessibility',
+    '/services/seo',
+    '/services/educations',
+    '/services/web-development',
+    '/about-us',
+    '/contact',
+    '/join-network'
   ]
+
+  const languages = ['sv', 'en']
+
+  const urls = []
+
+  for (const lang of languages) {
+    for (const route of routes) {
+      urls.push({
+        url: `${baseUrl}/${lang}${route}`,
+        lastModified: new Date(),
+      })
+    }
+  }
+
+  return urls
 }
