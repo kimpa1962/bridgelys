0a9383c0202ed4f64aaaae71c278c16514210872
Kim <hello@kimpa.se>
2026-03-20 21:40:12 +0100

    remove locale prefix

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,5 +2,6 @@ import {defineRouting} from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["sv", "en"],
-  defaultLocale: "sv"
+  defaultLocale: "sv",
+  localePrefix: "never"
 });
